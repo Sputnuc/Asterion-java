d808edaef6753c0b1ee805c59cac0f4ee97e2ec9
AnDashik (Andrey Kozlov) <mr.andrekozlov@gmail.com>
2025-07-21 11:44:34 +0300

    Add EffectFloor class

---
+++ src/AsJava/world/blocks/environment/EffectFloor.java
@@ -0,0 +1,33 @@
+package AsJava.world.blocks.environment;
+/* Yoinked from Psammos :3 (Andromeda if you're reading this, please don't hit me) */
+
+import arc.graphics.Color;
+import arc.math.Mathf;
+import mindustry.content.Blocks;
+import mindustry.content.Fx;
+import mindustry.entities.Effect;
+import mindustry.graphics.Pal;
+import mindustry.world.Tile;
+import mindustry.world.blocks.environment.Floor;
+
+public class EffectFloor extends Floor {
+    public Effect effect = Fx.fire;
+    public Color effectColor = Pal.vent;
+    public float effectChance = 1;
+
+    public EffectFloor(String name){
+        super(name);
+    }
+
+    @Override
+    public boolean updateRender(Tile tile) {
+        return true;
+    }
+
+    @Override
+    public void renderUpdate(UpdateRenderState state) {
+        if(Mathf.chanceDelta(effectChance) && state.tile.block() == Blocks.air) {
+            effect.at(state.tile.worldx(), state.tile.worldy(), effectColor);
+        }
+    }
+}
